e505b385afc0ba9648b4e611e5de75a6b14a01fb
Alexander8123 <francisco.alexandre.silva99@aluno.ifce.edu.br>
2026-06-09 14:02:53 -0300

    add control buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,32 @@
+import Button from "@/components/Button";
 import { Character } from "@/components/Candle/Character";
+
 import { StyleSheet, View } from "react-native";
 
 export default function Home() {
     return (
         <View style={styles.container}>
             <Character isBlink={false} />
+            <View style={styles.containerButtons}>
+                <Button
+                    text="Start"
+                    background="#0f0"
+                    textSize={20}
+                    textColor="#fff"
+                    paddingX={20}
+                    paddingY={10}
+                    rounded={999}
+                />
+                <Button
+                    text="Stop"
+                    background="#f00"
+                    textSize={20}
+                    textColor="#fff"
+                    paddingX={20}
+                    paddingY={10}
+                    rounded={999}
+                />
+            </View>
         </View>
     )
 }
@@ -16,4 +38,8 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center"
     },
+    containerButtons: {
+        flexDirection: "row",
+        marginTop: 20
+    }
 });
