9efe52bca70a550e7dbc23a165539df3afd5ca16
Alexander8123 <francisco.alexandre.silva99@aluno.ifce.edu.br>
2026-06-09 14:14:35 -0300

    add play control buttons

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,27 @@
 import Button from "@/components/Button";
 import { Character } from "@/components/Candle/Character";
+import { useState } from "react";
 
 import { StyleSheet, View } from "react-native";
 
 export default function Home() {
+    const [start, setStart] = useState(false);
+
     return (
         <View style={styles.container}>
             <Character isBlink={false} />
             <View style={styles.containerButtons}>
                 <Button
-                    text="Start"
-                    background="#0f0"
+                    text={start ? "Pause" : "Start"}
+                    background={start ? "rgb(201, 201, 16)" : "#0f0"}
                     textSize={20}
                     textColor="#fff"
                     paddingX={20}
                     paddingY={10}
                     rounded={999}
+                    func={() => {
+                        setStart(prev => !prev)
+                    }}
                 />
                 <Button
                     text="Stop"
@@ -25,6 +31,9 @@ export default function Home() {
                     paddingX={20}
                     paddingY={10}
                     rounded={999}
+                    func={() => {
+                        setStart(false)
+                    }}
                 />
             </View>
         </View>
